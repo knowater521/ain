c9d665040e82713bb4fc269651ae1fc3846baf3b
DeFiDev M <defidev@cakedefi.com>
2020-07-29 17:27:49 +0700

    autodismiss/revert expired orders; orders test; some modified rpc getters output

--- src/masternodes/orders.cpp
+++ src/masternodes/orders.cpp
@@ -6,6 +6,7 @@
 
 /// @attention make sure that it does not overlap with those in masternodes.cpp/tokens.cpp/undos.cpp/accounts.cpp !!!
 const unsigned char COrdersView::ByCreationTx::prefix = 'R';
+const unsigned char COrdersView::ByExpiryHeight::prefix = 'e';
 
 boost::optional<COrder> COrdersView::GetOrder(uint256 const & orderTx) const
 {
@@ -19,14 +20,35 @@ void COrdersView::ForEachOrder(std::function<bool(uint256 const & orderTx, COrde
     }, start);
 }
 
+void COrdersView::ForEachExpiredOrder(std::function<bool (const uint256 &)> callback, uint32_t expiryHeight) const
+{
+    ForEach<ByExpiryHeight, ExpiredKey, char>([&] (ExpiredKey key, char) {
+        if (key.height <= expiryHeight)  // 'equal' cause called for current blockheight, AFTER applying block txs (orders' job complete)
+            return callback(key.orderTx);
+        else
+            return false;
+    }, ExpiredKey{0, uint256()}); // from the very beginning!
+}
+
 Res COrdersView::DelOrder(uint256 const & orderTx)
 {
-    EraseBy<ByCreationTx>(orderTx);
+    auto const order = GetOrder(orderTx);
+    if (order) {
+        EraseBy<ByCreationTx>(orderTx);
+        if (order->timeInForce) {
+            EraseBy<ByExpiryHeight>(ExpiredKey{(uint32_t)(order->creationHeight + order->timeInForce), orderTx});
+        }
+    }
     return Res::Ok();
 }
 
 Res COrdersView::SetOrder(uint256 const & orderTx, COrder const & order)
 {
     WriteBy<ByCreationTx>(orderTx, order);
+    if (order.timeInForce) {
+        WriteBy<ByExpiryHeight>(ExpiredKey{(uint32_t)(order.creationHeight + order.timeInForce), orderTx}, '\0');
+    }
     return Res::Ok();
 }
+
+
